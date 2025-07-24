574d22f166c8159905d27314332a31c04db9e326
lucky <lucky268angel@gmail.com>
2025-07-24 20:09:25 +0800

    基于服务器的证书和服务器名字就可以对服务器进行验证

--- pubsub-proto-client-sub/main.go
+++ pubsub-proto-client-sub/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 
 	pb "github.com/lucky2688/gotest/pubsub-proto/protobuf"
@@ -10,7 +11,20 @@ import (
 
 // gRPC 客户端 订阅
 func main() {
-	conn, err := grpc.Dial("localhost:1234", grpc.WithInsecure())
+	//conn, err := grpc.Dial("localhost:1234", grpc.WithInsecure())  无证书
+
+	//基于服务器的证书和服务器名字就可以对服务器进行验证
+	creds, err := credentials.NewClientTLSFromFile(
+		"../pubsub-proto/server.crt", "server.grpc.io",
+	)
+	if err != nil {
+		log.Fatal("TLS error:", err)
+	}
+
+	conn, err := grpc.Dial("localhost:1234",
+		grpc.WithTransportCredentials(creds),
+	)
+
 	if err != nil {
 		log.Fatal("Failed to connect:", err)
 	}
--- pubsub-proto/main.go
+++ pubsub-proto/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 	"strings"
@@ -53,7 +54,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	grpcServer := grpc.NewServer()
+	//grpcServer := grpc.NewServer()  无证书
+
+	//有证书
+	creds, err := credentials.NewServerTLSFromFile("server.crt", "server.key")
+	if err != nil {
+		log.Fatal("TLS error:", err)
+	}
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
+
 	pb.RegisterPubsubServiceServer(grpcServer, NewPubsubService())
 
 	log.Println("gRPC Pubsub server started on :1234")
@@ -61,3 +70,25 @@ func main() {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
+
+/*
+windows下用cmd或者powershell执行生成证书
+自 Go 1.15+ 和 gRPC 使用的 crypto/x509 模块开始，证书校验默认要求包含 SAN
+# 生成私钥
+openssl genrsa -out server.key 2048
+# 生成带 SAN 的证书
+openssl req -x509 -new -nodes -key server.key -days 3650 -out server.crt -config cert.conf
+
+
+到main.go和证书同一级目录下执行
+PS C:\projectgo\gotest> cd .\pubsub-proto\
+PS C:\projectgo\gotest\pubsub-proto> go run main.go
+2025/07/24 19:08:56 gRPC Pubsub server started on :1234
+
+
+
+以上这种方式，需要提前将服务器的证书告知客户端，这样客户端在连接服务器时才能进行对服务器证书认证。
+在复杂的网络环境中，服务器证书的传输本身也是一个非常危险的问题。
+如果在中间某个环节，服务器证书被监听或替换那么对服务器的认证也将不再可靠。
+
+*/
